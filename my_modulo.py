8c3bdd0cdf1a3280076920d6df58d4515dbf98b5
oscarcorres <ocorres@gmail.com>
2018-04-24 10:15:36 +0200

    ficheros de condigo añadidos

--- my_modulo.py
+++ my_modulo.py
@@ -5,7 +5,7 @@
 #
 # Perpetrados por: Oscar Corres y Viloria
 
-# 4-  Crea una función crea_listado que sea capaz de crear un listado de n elementos y devolverlo.
+# 4-  Crear una función crea_listado que sea capaz de crear un listado de n elementos y devolverlo.
 #     N será un parámetro que pasarás a la función, será 0 por defecto. Que recibirá otro parámetroç
 #     que deberá indicar el valor inicial de cada elemento del listado, que será 0 por defecto.
 #     Esta función deberá estar presente en el módulo.
